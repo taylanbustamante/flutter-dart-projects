1e5a70352e693726b92c224f0ff2db3ec8bead00
Taylan Bustamante <taylanbustamante@gmail.com>
2023-03-16 13:12:58 -0300

    Project initiated

---
+++ expenses/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: const Center(
+        child: Text("Versão Inicial"),
+      ),
+    );
+  }
+}
